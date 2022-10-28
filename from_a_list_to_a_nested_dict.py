9e6c6a321e3d103d1e0a00b552b0212440004b23
yahooj2020 <62124198+yahooj2020@users.noreply.github.com>
2022-10-28 14:30:53 +0900

    Update from_a_list_to_a_nested_dict.py

--- from_a_list_to_a_nested_dict.py
+++ from_a_list_to_a_nested_dict.py
@@ -2,15 +2,24 @@
 import string
 list_content=  list(string.ascii_uppercase)
 
+print("\n")
+print("\n")
+
+
+
 print(list_content)
-
-print("-"*600)
-
+print("\n")
+print("\n")
+print("-"*100)
+print("\n")
+print("\n")
 def nested_dict(list_content):
     if list_content:
-        return {list_content[0]: build_tree(list_content[1:])}
+        return {list_content[0]: nested_dict(list_content[1:])}
 
 print(nested_dict(list_content))
+print("\n")
+print("\n")
 
 # https://stackoverflow.com/questions/40401886/how-to-create-a-nested-dictionary-from-a-list-in-python
 
